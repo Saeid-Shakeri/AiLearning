66f9449149d51de0e83675ee4d541b8abb27b528
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-03-16 10:40:42 +0330

    customize admin panel

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Course, Lesson, Category, Professor
 # Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    exclude = ('score')
+
+@admin.register(Lesson)
+class CourseAdmin(admin.ModelAdmin):
+    exclude = ('score')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('score',)
+
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    
+    exclude = ('score',)
