4305bfeb4167bbaff4a0f1b0140a23f2a1a7390b
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-03-26 01:57:46 +0430

    add inlineLesson

--- course/admin.py
+++ course/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from .models import Course, Lesson, Category, Professor
 # Register your models here.
 
+
+class InlineLesson(admin.StackedInline):
+    model = Lesson
+    extra = 0
+    exclude = ('score',)
+    
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    inlines = [InlineLesson]
     exclude = ('score',)
     search_fields = ['name']
+
 
 
 @admin.register(Lesson)
