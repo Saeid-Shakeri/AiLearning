22207a3366680fdc02bdf23b6c4efc4844894a69
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-04-09 03:51:46 +0430

    edit urls

--- publication/urls.py
+++ publication/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('publications/', ArticleListView.as_view(), name='article_lisr'),
-    path('publication/details/<slug:slug>/', ArticleDetailView.as_view(), name='article_details'),
+    path('articles/', ArticleListView.as_view(), name='article_list'),
+    path('article/details/<slug:slug>/', ArticleDetailView.as_view(), name='article_details'),
    
 
 ]
