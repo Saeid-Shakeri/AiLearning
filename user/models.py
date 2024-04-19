9c2708c95517ec770a21d66cd2f7ed152e14b73e
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-04-19 11:02:19 +0430

    add str method

--- user/models.py
+++ user/models.py
@@ -10,8 +10,8 @@ class User(AbstractUser):
         regex=r'^(\+98|0)?9\d{9}$',
         message="Phone number must be entered in the format: '+989999999999' or '09999999999'. "
     )
-    phone = models.CharField(validators=[phone_regex], max_length=14, blank=False)
-    email = models.EmailField(max_length=50,blank=True,null=True)
+    phone = models.CharField(validators=[phone_regex], max_length=14)
+    email = models.EmailField(max_length=50,blank=True,null=True,default='')
 
     def __str__(self) -> str:
         return f"{self.username}"
@@ -21,4 +21,9 @@ class Attend(models.Model):
     user = models.ForeignKey(to=User,on_delete=models.CASCADE)
     course = models.ForeignKey(to=Course,on_delete=models.PROTECT)
     progress = models.DecimalField(max_digits=3,default=0,decimal_places=3)
+
+    
+    def __str__(self) -> str:
+        return f"{self.user}--{self.course}"
+
         
