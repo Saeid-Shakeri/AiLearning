4c5591286c76a972755a4cdf1805482aa077c276
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-04-19 11:05:30 +0430

    add add_course view

--- course/views.py
+++ course/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, View
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
 
 from .models import Course, Category
 from publication.models import Article
+from user.models import Attend
 # Create your views here.
 
 
@@ -11,19 +15,31 @@ class CourseListView(ListView):
     context_object_name = 'course_list'
     queryset = Course.objects.order_by('-date')
     template_name = 'course/course_list.html'
-    #paginate_by = 5
+    # paginate_by = 5
 
 
 
-class CourseDetailView(View):
     
-    def get(self, request, slug):
-        course = Course.objects.get(slug=slug)
-        prof = course.professor.all()
-        context={
-            'course':course, 'prof': prof
-        }
-        return render(request,'course/course_detail.html',context)
+def CourseDetailView(request, slug):
+    course = Course.objects.get(slug=slug)
+    prof = course.professor.all()
+    context={
+        'course':course, 'prof': prof
+    }
+    return render(request,'course/course_detail.html',context)
 
 
+@login_required(login_url='/user/login/')
+def add_course(request, **kwargs):
+    user = request.user
+    course_id = request.POST['course_id']
+    course = Course.objects.get(id=course_id)
+    user_objects = Attend.objects.filter(user=user)
+    for c in user_objects:
+        if c.course == course:
+            return HttpResponse('you attended in this course')
+    
+    Attend.objects.create(user=user,course=course)
+    return HttpResponse('the course added to your profile')
+        
     
