f354cb82d27354be7265cbb3ab6c4ff0252ec020
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-05-19 02:08:22 +0330

    send user.id to index.html

--- AiLearning/views.py
+++ AiLearning/views.py
@@ -9,7 +9,8 @@ def index(request):
     if request.method == 'GET':
         context ={}
         context["category"] = Category.objects.all()
-        context["course"] = Course.objects.order_by('-date')[:3]
+        context["course"] = Course.objects.order_by('-date')[:5]
         context["article"] = Article.objects.order_by('-date')[:3]
+        context["user"] = request.user.id
         return render(request, "AiLearning/index.html", context)
         
