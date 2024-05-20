2018b2925bbaff76e8a6d029a19f8d7f6a62c58a
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-05-20 19:38:45 +0330

    change author name

--- course/views.py
+++ course/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, View, DetailView
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+# from django.http impor
 
 
 from .models import Course, Category, Lesson
