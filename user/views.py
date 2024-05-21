83602019880d1567731b29cf6cd96d39ccdf4f43
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-05-21 17:41:47 +0330

    Unify signup & login pages

--- user/views.py
+++ user/views.py
@@ -14,25 +14,32 @@ def signup(request):
     if request.method == 'GET':
         if request.user.is_authenticated:
             return redirect("index")
-        return render(request, 'user/signup.html', {'user':request.user.id})
+        return render(request, 'user/signup_login.html', {'user':request.user.id})
 
 
     elif request.method == 'POST':
         try : 
             username = request.POST.get('username')
             u = User.objects.filter(username=username)
-            if username in u :
-                message = 'این نام کاربری موجود است لطفا یک نام دیگر را امتحان کنید.'
-                return render(request,'user/signup.html',{'message':message,'user':request.user.id})
+            for i in u:
+                if username == i.username:
+                    message = 'این نام کاربری موجود است لطفا یک نام دیگر را امتحان کنید.'
+                    return render(request,'user/signup_login.html',{'message':message,'user':request.user.id})
+            email = request.POST.get('email')
+            u = User.objects.filter(email=email)
+            for i in u :
+                if email == i.email :
+                    message = 'این ایمیل موجود است لطفا یک ایمیل دیگر را امتحان کنید.'
+                    return render(request,'user/signup_login.html',{'message':message,'user':request.user.id})
             password1 = request.POST.get('password1')
-            if len(str(password1)) < 8:
-                message = 'رمز عبور نباید کمتر از 8 کاراکتر باشد. یک رمز دیگر بسازید.'
-                return render(request,'user/signup.html',{'message':message,'user':request.user.id})
+            # if len(str(password1)) < 8:
+            #     message = 'رمز عبور نباید کمتر از 8 کاراکتر باشد. یک رمز دیگر بسازید.'
+            #     return render(request,'user/signup_login.html',{'message':message,'user':request.user.id})
             password2 = request.POST.get('password2')
             if password1 != password2 :
                 message = 'رمز عبور مطابقت ندارد دوباره تلاش کنید.'
-                return render(request,'user/signup.html',{'message':message,'user':request.user.id})
-            user = User.objects.create(username=username)
+                return render(request,'user/signup_login.html',{'message':message,'user':request.user.id})
+            user = User.objects.create(username=username,email=email)
             user.set_password(password1)
             user.save()
             login(request, user)
@@ -40,7 +47,7 @@ def signup(request):
         except Exception as e:
             message = 'A problem occurred. please try again later'
             logger.warning(f'signup view: {str(e)}')
-            return render(request,'user/signup.html',{'message':message,'user':request.user.id})
+            return render(request,'user/signup_login.html',{'message':message,'user':request.user.id})
 
 
 
@@ -50,25 +57,31 @@ def Login(request):
     if request.method == 'GET':
         if request.user.is_authenticated:
             return redirect("index")
-        return render(request, 'user/login.html', {'user':request.user.id})
+        return render(request, 'user/signup_login.html', {'user':request.user.id})
 
     elif request.method == 'POST':
         try: 
-            username = request.POST.get('username')
+            email_username = request.POST.get('email_username')
             password = request.POST.get('password')
 
             authed_user = authenticate(
-                        username=username, password=password)
+                        username=email_username, password=password)
+            if authed_user:
+                login(request, authed_user)
+                return redirect('dashboard')
+            u = User.objects.filter(email=email_username)
+            authed_user = authenticate(
+                        username=u[0].username, password=password)
             if authed_user:
                 login(request, authed_user)
                 return redirect('dashboard')
             else :
                 message = 'رمز عبور یا نام کاربری اشتباه است. دوباره تلاش کنید'
-                return render(request,'user/login.html',{'message':message, 'user':request.user.id})
+                return render(request,'user/signup_login.html',{'message':message, 'user':request.user.id})
         except Exception as e:
             message = 'A problem occurred. please try again later'
             logger.warning(f'Login view: {str(e)}')
-            return render(request,'user/login.html',{'message':message,'user':request.user.id})
+            return render(request,'user/signup_login.html',{'message':message,'user':request.user.id})
 
 
 
