dab283ac1d14572ac4b009f6e0d9857e07aaa0f2
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-06-18 06:11:39 +0330

    add continue_course url

--- course/urls.py
+++ course/urls.py
@@ -6,11 +6,9 @@ urlpatterns = [
     path('courses/', CourseListView.as_view(), name='courses'),
     path('details/<slug:slug>/', CourseDetailView, name='course_details'),
     path('details/<slug:slug>/addcourse/',add_course , name='addcourse'),
+    path('details/<slug:slug>/continue-course/', continue_course, name='continue_course'),
     path('categories/<slug:slug>/', category_courses, name='category_courses'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
     path('lesson/<slug:slug>/', lesson, name='lesson'),
-    path('rate-course/', rate_course, name='rate_course'),
-
-   
 
 ]
