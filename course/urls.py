25dc8527f54dae3bc72933dc309c83d90af4ec49
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-06-18 22:13:00 +0330

    some changes

--- course/urls.py
+++ course/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('courses/', CourseListView.as_view(), name='courses'),
     path('details/<slug:slug>/', CourseDetailView, name='course_details'),
     path('details/<slug:slug>/addcourse/',add_course , name='addcourse'),
-    path('details/<slug:slug>/continue-course/', continue_course, name='continue_course'),
+    path('details/<slug:slug>/continue_course/', continue_course, name='continue_course'),
     path('categories/<slug:slug>/', category_courses, name='category_courses'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
-    path('lesson/<slug:slug>/', lesson, name='lesson'),
+    path('lesson/<slug:slug>/', get_lesson, name='get_lesson'),
 
 ]
