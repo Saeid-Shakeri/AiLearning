b8efe3bc4e03fa58f97265534f2d2ce044f65da0
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-06-24 05:46:38 +0330

    show answer, question

---
+++ static/js/show.js
@@ -0,0 +1,15 @@
+function show(name) {
+    console.log('kfdnvojeuhbwovb')
+    if (name == "answer"){
+        let x = document.forms[name]["answer"].value;
+        alert(x)
+        return false
+    }
+    else if( name == "question"){
+        let x = document.forms[name]["question"].value;
+        alert(x)
+        return false
+
+    }
+
+}
