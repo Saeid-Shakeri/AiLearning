a292ce32c3fee19d817f98939dcf421d56a5d8f3
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-06-24 06:36:37 +0330

    feat: logers & jalali_date

--- AiLearning/settings.py
+++ AiLearning/settings.py
@@ -77,32 +77,32 @@ JAZZMIN_SETTINGS = {
     "changeform_format_overrides": {"user.User": "collapsible"},
 }
 
+
 JAZZMIN_UI_TWEAKS = {
-    "theme": "cyborg",
-    "dark_mode_theme": "darkly",
     "navbar_small_text": True,
     "footer_small_text": True,
     "body_small_text": False,
     "brand_small_text": False,
     "brand_colour": "navbar-gray",
-    "accent": "accent-primary",
-    "navbar": "navbar-info navbar-gray",
+    "accent": "accent-success",
+    "navbar": "navbar-teal navbar-dark",
     "no_navbar_border": False,
     "navbar_fixed": False,
     "layout_boxed": False,
     "footer_fixed": False,
     "sidebar_fixed": True,
-    "sidebar": "sidebar-dark-purple",
+    "sidebar": "sidebar-dark-olive",
     "sidebar_nav_small_text": True,
     "sidebar_disable_expand": False,
     "sidebar_nav_child_indent": False,
     "sidebar_nav_compact_style": False,
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": False,
-  
+    "theme": "flatly",
+    "dark_mode_theme": "darkly",
     "button_classes": {
-        "primary": "btn-outline-primary",
-        "secondary": "btn-outline-secondary",
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
         "info": "btn-outline-info",
         "warning": "btn-outline-warning",
         "danger": "btn-outline-danger",
@@ -110,7 +110,6 @@ JAZZMIN_UI_TWEAKS = {
     },
     "actions_sticky_top": True
 }
-
 """
 Django settings for AiLearning project.
 
@@ -156,6 +155,7 @@ INSTALLED_APPS = [
     'course',
     'publication',
     'user',
+	'jalali_date',
 ]
 
 MIDDLEWARE = [
@@ -181,6 +181,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'django.core.context_processors.request',
             ],
         },
     },
@@ -237,6 +238,14 @@ LOGGING = {
     "loggers": {
         "user":{
             "handlers": ["file"],
+        },
+        "ailearning":{
+            "handlers": ["file"],
+
+        },
+        "course":{
+            "handlers": ["file"],
+
         },
     },
    "formatters": {
