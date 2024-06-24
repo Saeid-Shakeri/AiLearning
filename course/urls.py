c5a99dee764ee1f4dfb0abff10c2c46ffff52b0d
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-06-24 05:44:25 +0330

    add like and dislike urls

--- course/urls.py
+++ course/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('categories/<slug:slug>/', category_courses, name='category_courses'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
     path('lesson/<slug:slug>/', get_lesson, name='get_lesson'),
+    path('like_coursecomment/<int:id>/',like_coursecomment,name="like_coursecomment"),
+    path('dislike_coursecomment/<int:id>/',dislike_coursecomment,name="dislike_coursecomment"),
+    path('like_lessoncomment/<int:id>/',like_lessoncomment,name="like_lessoncomment"),
+    path('dislike_lessoncomment/<int:id>/',dislike_lessoncomment,name="dislike_lessoncomment"),
 
 ]
--- publication/urls.py
+++ publication/urls.py
@@ -6,6 +6,10 @@ from .views import *
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name='article_list'),
     path('article/details/<slug:slug>/', ArticleDetailView.as_view(), name='article_details'),
+    path('like_articlecomment/<int:id>/',like_articlecomment, name='like_articlecomment'),
+    path('dislike_articlecomment/<int:id>/',dislike_articlecomment,  name='dislike_articlecomment'),
+
+
    
 
 ]
