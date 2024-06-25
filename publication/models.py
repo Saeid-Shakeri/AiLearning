0aeb0dccb9ef7ed40338e92b537a11d138e0b329
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-06-25 05:51:56 +0330

    some changes

--- publication/models.py
+++ publication/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     name = models.CharField(max_length=150,help_text='The titile of Article')
     body = models.TextField( null=True, blank=True)
     author = models.ManyToManyField(to=Professor)
-    date = models.DateField()
+    date = models.DateTimeField()
     file = models.FileField(null=True,blank=True,upload_to='media/article/file/')
     image = models.ImageField(null=True,blank=True,upload_to='media/article/image/')
     category = models.ForeignKey(to=Category,on_delete=models.PROTECT,null=True)
@@ -38,33 +38,34 @@ class Article(models.Model):
     
 
 class ArticleRates(models.Model):
-    user = models.ForeignKey(to=User,on_delete=models.PROTECT)
+    user = models.ForeignKey(to=User,on_delete=models.SET_NULL,null=True)
     article = models.ForeignKey(to=Article,on_delete=models.CASCADE)
 
 
 class ArticleComment(models.Model):
     name = models.CharField(max_length=50)
-    content = models.CharField(max_length=250, null=True, help_text="Comment text")
+    content = models.TextField(help_text="Comment text")
     email = models.EmailField()
-    reply = models.ForeignKey('self',on_delete=models.CASCADE,null=True, blank=True)
+    # reply = models.ForeignKey('self',on_delete=models.CASCADE,null=True, blank=True)
+    checked = models.BooleanField(default=False)
     article = models.ForeignKey(to=Article,on_delete=models.PROTECT, null=True, blank=True)
     date_added = models.DateField(auto_now_add=True)
-    rates = models.PositiveIntegerField(default=0)
+    # rates = models.PositiveIntegerField(default=0)
     likes = models.ManyToManyField(to=User,related_name="article_likes",null=True,blank=True)
     dislikes = models.ManyToManyField(to=User,related_name="article_dislikes",null=True,blank=True)
-    score = models.FloatField(default=0,
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(5),
-        ]
-    )
+    # score = models.FloatField(default=0,
+    #     validators=[
+    #         MinValueValidator(0),
+    #         MaxValueValidator(5),
+    #     ]
+    # )
 
     class Meta:
         verbose_name = 'Article Comment'
         verbose_name_plural = 'Article Comments'
 
     def __str__(self):
-        return f"Comment: {self.content}"
+        return f"{self.name}____{self.article}____{self.checked}"
     
     def number_of_likes(self):
         return self.likes.count()
